68b0ee47c2e46a6797c3d92fc08a41a49c3894ff
David Mills <david_mills109@yahoo.com>
2020-09-14 20:06:16 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,16 @@
 def unsafe?(speed)
-
+  if speed < 40 || speed > 60
+    return true
+  else 
+    return false
+  end
 end
 
-
+  
+#*Going 30 mph on the freeway would be unsafe, as would going 95 mph. Going 50 miles per hour, however, would return `false` as that's within the "safe" range.*
 
 def not_safe?(speed)
-	
+   speed < 40 || speed > 60 ? true : false 
 end
 	
 
